b276697309d3901f4bf5de18343197142aca3d20
PancratiA <pancrati.adriana-maria.22@stud.umfst.ro>
2023-10-27 12:03:15 +0300

    A-R

---
+++ Test/TrueFalseQuestion.java
@@ -0,0 +1,19 @@
+package Test;
+
+public class TrueFalseQuestion extends Intrebare {
+
+    public TrueFalseQuestion(String intrebare, String raspunsCorect) {
+        super(intrebare,raspunsCorect);
+    }
+
+    @Override
+    public boolean verificaRaspuns(String raspuns) {
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "True/False: " + super.toString();
+    }
+}
